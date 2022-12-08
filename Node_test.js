bf736ee19284535a9225cf10c2f8bad6540643e2
raiadelrey <109860281+raiadelrey@users.noreply.github.com>
2022-12-07 22:25:44 -0300

    Add files via upload

---
+++ Node_test.js
@@ -0,0 +1,24 @@
+/*
+***Com base nos vídeos 1 e 2, explique como a web funciona a partir do ponto de vista 
+   do protocolo cliente servidor.
+
+  Ao acessar [o cliente] um endereço web, o computador interpreta o comando e envia uma solicitação get
+usando HTTP ao servidor, comunicando que o cliente está tentando aceessar àquele código HTML.
+Ou seja, os clientes enviam requisões HTTP e os servidores retornam respostas HTTP aos clientes.
+
+
+
+***Crie o começo de um projeto em node.js, usando o npm e informe os comandos utilizados.
+
+1º Cria-se uma pasta para o projeto.
+
+2º Abre-se a pasta e nela abre-se o terminal.
+
+3º No terminal, digita-se o comando "npm init", o qual irá permitir o início do projeto, 
+criando o package.json. Preechem-se os campos sobre o projeto, como o nome e o autor.
+
+4º Executa-se o comando "npm install" no terminal, o qual permitirá que todas as dependências 
+necessárias sejam instaladas. 
+
+*/
+
