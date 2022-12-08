62098fc6db7d5b43a8090f91f13c0c819d2e6ef3
raiadelrey <109860281+raiadelrey@users.noreply.github.com>
2022-12-08 16:13:09 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express()
+
+app.get('/', (req,res) => {
+    res.send ('Usando o Express, crie um código Node.JS, que deve receber requisições HTTP GET e POST. \n Depois, coloque o servidor para rodar na porta 8080.')
+})
+
+app.post('/', (req,res) => {
+    res.send ('Olá express')
+})
+
+
+app.listen('8080', ()=>
+    {console.log ("Servidor executado na porta 8080")
+
+    
+})
